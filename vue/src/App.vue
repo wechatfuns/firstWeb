fb66f58981841ceda4c0c17a0b6e2ae41b8cbb5f
wechatfuns <115338233+wechatfuns@users.noreply.github.com>
2023-02-04 08:35:40 +0800

    Add files via upload

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <div id="app">
-    <img class="logo" alt="CloudBase logo" src="./assets/cloudbase.png" />
-    <img class="logo" alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="欢迎使用云开发 CloudBase Vue.js App" />
+<div style="width: auto;height: 400px" id="main">
+	<h1>111王聪这是第一个空文档喔</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+import * as echarts from 'echarts';
+
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('main'));
+// 绘制图表
+myChart.setOption({
+    title: {
+        text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+});
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-.logo {
-  width: 120px;
-  padding: 10px;
-}
 </style>
